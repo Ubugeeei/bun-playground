81b073658a024e891c03ad8a4055e358e38477ba
Ubugeeei <ubuge1122@gmail.com>
2023-09-09 23:24:12 +0900

    loaders

---
+++ other-loaders/index.ts
@@ -0,0 +1,9 @@
+import toml from "./data.toml";
+import json from "./data.json";
+import txt from "./data.txt";
+import rs from "./lib.rs";
+
+console.log("toml:\n", toml, "\n");
+console.log("json:\n", json, "\n");
+console.log("txt:\n", txt, "\n");
+console.log("rs:\n", rs, "\n");
