30498cfeaa2545cc2afd07bd261818451156f27f
Ubugeeei <ubuge1122@gmail.com>
2023-09-10 17:14:39 +0900

    vitest

---
+++ test/vitest/math.spec.ts
@@ -0,0 +1,80 @@
+import { expect, test } from "vitest";
+
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(3 + 1).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
